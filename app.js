869f5f0d1b34d54193e01128e8b07a44bcef921d
Cao Quyết Thắng <thangcq98@gmail.com>
2022-01-12 15:40:25 +0700

    sign up when type /start

--- app.js
+++ app.js
@@ -19,55 +19,42 @@ const mongoDBConnection = require('./src/models/mongo_connection');
   //   // runs after next middleware finishes
   //   console.timeEnd(`Processing update ${ctx.update.update_id}`);
   // });
-  bot.start((ctx)=>{
+  bot.start(async (ctx)=>{
     // add non-exist user
-    ctx.reply('add non-exist user');
     let telegramUserId = ctx.message.from.id;
     let name = ctx.message.from.last_name+' '+ctx.message.from.first_name;
     let username = ctx.message.from.username;
     let userModel = mongoDBConnection.exportUserModel();
-    let query = {};
+    let query = {
+      userId: telegramUserId
+    };
     let update = {
       userId: telegramUserId,
       name: name,
       username: username,
     };
-    let options = { upsert: false, new: true, setDefaultsOnInsert: true };
-    userModel.findOneAndUpdate(query, update, options, (error, data)=>{
-      console.log(error);
-      console.log(data);
-    });
+    let options = { upsert: true, new: true, rawResult: true };
+    let res = await userModel.findOneAndUpdate(query, update, options);
+    if(res.lastErrorObject.updatedExisting){
+      ctx.reply('Your account already exists');
+    }else{
+      ctx.reply('Sign Up Success')
+    }
   })
-  bot.command('log', (ctx) => {
+  bot.command('log', async (ctx) => {
     let text = ctx.message.text;
     let log = text.split('/log')[1].trim();
     let timestamp = ctx.message.date;
     let detail = '';
     let telegramUserId = ctx.message.from.id;
     let taskLogModel = mongoDBConnection.exportTaskLogModel();
-    taskLogModel.create({
+    await taskLogModel.create({
       userId:telegramUserId,
       timestamp: timestamp,
       content: log,
       detail: detail,
     });
   })
-  bot.on('new_chat_participant', function(message) {
-    // 'message' is a Telegram Message
-  
-    // 'user' below is a Telegram User
-    const user = message['new_chat_participant'];
-    console.log(user);
-  });
-  
-  
-  bot.on('left_chat_participant', function(message) {
-    // 'message' is a Telegram Message
-  
-    // 'user' below is a Telegram User
-    const user = message['left_chat_participant'];
-    console.log(user);
-  });
   bot.launch();
   // Enable graceful stop
   process.once('SIGINT', () => bot.stop('SIGINT'));
