e49ed30f5cd7194e96f966845007814b2075ae84
Cao Quyết Thắng <thangcq98@gmail.com>
2022-01-12 17:36:19 +0700

    add fixed schedule to send reminding

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const { Telegraf } = require('telegraf');
+const nodeSchedule = require('node-schedule');
 const HttpsProxyAgent = require('https-proxy-agent')
 const process = require('process');
 const mongoDBConnection = require('./models/mongo_connection');
-(async ()=>{
+(async () => {
   await mongoDBConnection.initMongoDBConnection();
   const bot = new Telegraf(process.env.BOT_TOKEN, {
     telegram: {
@@ -12,17 +13,10 @@ const mongoDBConnection = require('./models/mongo_connection');
       }) : null,
     },
   });
-  
-  // bot.use(async (ctx, next) => {
-  //   console.time(`Processing update ${ctx.update.update_id}`);
-  //   await next(); // runs next middleware
-  //   // runs after next middleware finishes
-  //   console.timeEnd(`Processing update ${ctx.update.update_id}`);
-  // });
-  bot.start(async (ctx)=>{
+  bot.start(async (ctx) => {
     // add non-exist user
     let telegramUserId = ctx.message.from.id;
-    let name = ctx.message.from.last_name+' '+ctx.message.from.first_name;
+    let name = ctx.message.from.last_name + ' ' + ctx.message.from.first_name;
     let username = ctx.message.from.username;
     let userModel = mongoDBConnection.exportUserModel();
     let query = {
@@ -35,11 +29,20 @@ const mongoDBConnection = require('./models/mongo_connection');
     };
     let options = { upsert: true, new: true, rawResult: true };
     let res = await userModel.findOneAndUpdate(query, update, options);
-    if(res.lastErrorObject.updatedExisting){
+    if (res.lastErrorObject.updatedExisting) {
       ctx.reply('Your account already exists');
-    }else{
-      ctx.reply('Sign Up Success')
+    } else {
+      ctx.reply('Sign Up Success');
     }
+  })
+  bot.command('stop',async (ctx) => {
+    let telegramUserId = ctx.message.from.id;
+    let userModel = mongoDBConnection.exportUserModel();
+    let query = {
+      userId: telegramUserId
+    };
+    await userModel.findOneAndRemove(query);
+    ctx.reply('Remove account');
   })
   bot.command('log', async (ctx) => {
     let text = ctx.message.text;
@@ -49,14 +52,36 @@ const mongoDBConnection = require('./models/mongo_connection');
     let telegramUserId = ctx.message.from.id;
     let taskLogModel = mongoDBConnection.exportTaskLogModel();
     await taskLogModel.create({
-      userId:telegramUserId,
+      userId: telegramUserId,
       timestamp: timestamp,
       content: log,
       detail: detail,
     });
   })
+  let rule = new nodeSchedule.RecurrenceRule();
+  rule.dayOfWeek = [1,2,3,4,5];
+  rule.hour = [17,18,19,20,21,22,23];
+  // rule.hour = [8,10,14,16,17];
+  // rule.minute = 30;
+  rule.minute = [10,20,30,40,50,59];
+  nodeSchedule.scheduleJob(rule, ()=>{
+    loggingRemind(bot);
+  });
   bot.launch();
   // Enable graceful stop
   process.once('SIGINT', () => bot.stop('SIGINT'));
   process.once('SIGTERM', () => bot.stop('SIGTERM'));
 }).call();
+
+/**
+ * 
+ * @param {Telegraf} bot 
+ */
+async function loggingRemind(bot) {
+  let userModel = mongoDBConnection.exportUserModel();
+  let users = await userModel.find({});
+  for (const user of users) {
+    let teleId = user.userId;
+    bot.telegram.sendMessage(teleId,'What are you doing?');
+  }
+}
